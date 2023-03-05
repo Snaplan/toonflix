633d1bf396b3d5ed7196206d66422b55a68c3c9f
Snap_Lan <auraha78@gmail.com>
2023-03-05 13:40:30 +0900

    Episodes

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -44,39 +44,114 @@ class _DetailScreenState extends State<DetailScreen> {
               fontWeight: FontWeight.w600,
             )),
       ),
-      body: Column(children: [
-        const SizedBox(
-          height: 50,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Hero(
-              tag: widget.id,
-              child: Container(
-                width: 250,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.5),
-                      ),
-                    ]),
-                clipBehavior: Clip.hardEdge,
-                child: Image.network(
-                  widget.thumb,
-                  headers: const {
-                    "User-Agent":
-                        "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                  },
+      body: Padding(
+        padding: const EdgeInsets.all(50),
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: widget.id,
+                  child: Container(
+                    width: 250,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.5),
+                          ),
+                        ]),
+                    clipBehavior: Clip.hardEdge,
+                    child: Image.network(
+                      widget.thumb,
+                      headers: const {
+                        "User-Agent":
+                            "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                      },
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+            const SizedBox(
+              height: 15,
+            ),
+            FutureBuilder(
+              future: webtoon,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Text(
+                        "${snapshot.data!.genre} / ${snapshot.data!.age}",
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  );
+                }
+                return const Text("...");
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            FutureBuilder(
+              future: episodes,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [
+                      for (var episode in snapshot.data!)
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(
+                                  color: Colors.green,
+                                  style: BorderStyle.solid,
+                                  width: 5)),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 20,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  episode.title,
+                                  style: TextStyle(
+                                    color: Colors.green.shade500,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.chevron_right_rounded,
+                                  color: Colors.green.shade500,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+                }
+                return Container();
+              },
+            )
+          ]),
         ),
-      ]),
+      ),
     );
   }
 }
